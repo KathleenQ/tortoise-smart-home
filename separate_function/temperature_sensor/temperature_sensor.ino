bd7076a41806a9916f9571a7a379610abd691c81
Siying <u6099927@anu.edu.au>
2019-01-29 02:43:13 +1100

    Add OLED screen module to completed "temperature" function

--- separate_function/temperature_sensor/temperature_sensor.ino
+++ separate_function/temperature_sensor/temperature_sensor.ino
@@ -1,57 +1,28 @@
-/********************
- * (Jan29) [for "Temperature"]
- * Use of Water-proof Temperature Sensor & OLED
- * In the "Temperature" function, we will get temperature sensor data of water and show it on the OLED screen,
- * we will later send real-time message to the website, to let owner take further actions themselves (open heater remotely...).
- * (OLED brings convenience for daily life, if the owner can read water temperature immediately ~)
- * 
- * Temperature sensor:
+/****************
+ * (Jan26) Use of Water-proof Temperature Sensor [Temperature]
+ * In the "Temperature" function, we will only get temperature sensor data of water,
+ * and later send real-time message to the website, to let owner take further actions themselves (open heater remotely...).
  * Line Connection: S->digital port, '-'->GND, '+'->5V.
  * REFERENCE: "example1_Arduino" in http://www.alsrobot.cn/wiki/index.php/SKU:RB-02S160_DS18B20_%E6%B0%B4%E6%B8%A9%E4%BC%A0%E6%84%9F%E5%99%A8
- * OLED:
- * REFERENCE: https://randomnerdtutorials.com/guide-for-oled-display-with-arduino/
- * Line Connection: VCC->3.3V, GND, SCL, SDA.
- ********************/
-
-#include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
+****************/
 
 unsigned char temp_reset();
 void temp_write(char WRT);
 unsigned char temp_read();
 
-#define OLED_RESET 4
-Adafruit_SSD1306 display(OLED_RESET);
-const int tempPin = 12;
+const int tempPin = 12; //digital port 12
 float temperature = 0.0;
 
-void setup()
-{
-  Wire.begin();
-  display.begin(SSD1306_SWITCHCAPVCC, 0x3C); //initialize with the I2C addr 0x3C (for the 128x32)(initializing the display)
-  Serial.begin(9600); //(?)
+void setup() {
+  Serial.begin(9600); //further test(?)
 }
 
-void displayTemp(){
-  delay(2000); //(?) website test~
-  display.clearDisplay();
-  display.setTextColor(WHITE);
-  display.setTextSize(1);
-  display.setCursor(0,5);
-  display.print("Water Temperature:");
-  display.setTextSize(2);
-  display.setCursor(0,15);
-  display.print(temperature);
-  display.print(" C");
-}
-void loop()
-{
+void loop() {
   if(!temp_reset())
   {
     temp_write(0xCC); //"Skip_ROM"
     temp_write(0x44); //"Convert_T"
-    delay(750); //test(?)
+    delay(750); //need to test for delay time(?)
     temp_reset();
     temp_write(0xCC); //"Skip_ROM"
     temp_write(0xBE); //"Read_scratchpad"
@@ -59,12 +30,10 @@ void loop()
     unsigned short tempH = temp_read();
     unsigned int temp = ((unsigned int)tempH << 8) + (unsigned int)tempL;
     temp = (float)temp * 6.25;
-    temperature = (float)temp / 100; //Get the REAL "temperature" value~
+    temperature = (float)temp / 100;
     Serial.print("Temperature:");
     Serial.println(temperature);
   }
-  displayTemp();
-  display.display();
 }
 
 // All HELPER methods~
