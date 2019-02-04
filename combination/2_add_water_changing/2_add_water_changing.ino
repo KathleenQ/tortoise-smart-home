44a98c87c82b62c3df54b41871c94673b4e394a0
Siying <u6099927@anu.edu.au>
2019-02-05 01:19:58 +1100

    Add "real-time clock" for new board (without test)

--- combination/2_add_water_changing/2_add_water_changing.ino
+++ combination/2_add_water_changing/2_add_water_changing.ino
@@ -1,21 +1,22 @@
 /****************
- * (Feb04 1) Add the 2nd function: Water Changing (combine "_water_changing" file)
- * Use: Water-proof temperature sensor, Turbidity sensor, Pump (with MOS module),
- *      Real-time clock module.
- * 
- * Brief Explanation:
- * We will use real time to seperate various functions~ 
- * Temp: We will get data from water temperature sensor, show it on OLED screen, 
- * and send real-time message to the website, to let owner take further actions themselves.
- * Water Change: If more turbid than "normal level" (?), turn ON the pump.
- * (More turbid, output VALUE is smaller.)
- * 
- * Line CONNECTION: 
- * (OLED: VCC->3.3V;) Turbidity Sensor:Black->GND,Red->5V,Blue->AnalogIn;
- * Pump with MOS module:{smaller}'+'->I/O,{larger}'+'->5V,{both}'-'->GND;
- * Real-time clock: rtc(~CLK, I/O, RST) (e.g. rtc(6, 7, 8)).
+   (Feb05) Add the 2nd function: Water Changing (combine "_water_changing" & "realTime_clock_RtcDS1302")
+   Use: Water-proof temperature sensor, Turbidity sensor, Pump (with MOS module),
+        Real-time clock module.
+
+   Brief Explanation:
+   We will use real time to seperate various functions~
+   Temp: We will get data from water temperature sensor, show it on OLED screen,
+   and send real-time message to the website, to let owner take further actions themselves.
+   Water Change: If more turbid than "normal level" (?), turn ON the pump.
+   (More turbid, output VALUE is smaller.)
+
+   Line CONNECTION:
+   (OLED: VCC->3.3V;) Turbidity Sensor:Black->GND,Red->5V,Blue->AnalogIn;
+   Pump with MOS module:{smaller}'+'->I/O,{larger}'+'->5V,{both}'-'->GND;
+   Real-time clock: myWire(I/O, ~CLK, RST).
  ****************/
-//"WiFi Connection"
+
+//WiFi Connection
 #include <SPI.h>
 #include <WiFiNINA.h>
 #include "wifi_secret.h"
@@ -24,43 +25,65 @@
 //#include <Adafruit_GFX.h>
 //#include <Adafruit_SSD1306.h>
 //Real-time Clock
-//#include <MyRealTimeClock.h>
-#include <RTClib.h>
+#include <ThreeWire.h>
+#include <RtcDS1302.h>
 
 unsigned char temp_reset();
 void temp_write(char WRT);
 unsigned char temp_read();
-//String weekDay(uint8_t dayofweek);
 
-//"WiFi Connection"
+
+//WiFi Connection
 char ssid[] = SECRET_SSID; // CHANGE actual ssid & password in "wifi_secret.h"
 char pass[] = SECRET_PASS;
 int keyIndex = 0; //your network key Index number (needed only for WEP)
 int status = WL_IDLE_STATUS;
 WiFiServer server(80);
+
 //"Temperature"
 //#define OLED_RESET 4
 //Adafruit_SSD1306 display(OLED_RESET);
 const int tempPin = 12;
 float temperature = 0.0;
+
 //"Water Changing"
 const int turbidityPin = A1;
 const int pumpPin = 11;
 const float turbidityStandard = 1.2; //(?) Check later! Ideally(scientifically) set "< 4.1-0.3=3.8 V".
 float turbidityVol = 0.0;
-boolean pumpOn = false; //use for website condition shown\
+boolean pumpOn = false; //use for website condition shown
+
+//Real-time Clock
+ThreeWire myWire(7, 6, 8); // Connection: I/O, CLK, RST
+RtcDS1302<ThreeWire> Rtc(myWire);
+
 
 void setup() {
-//  Wire.begin();
-//  display.begin(SSD1306_SWITCHCAPVCC, 0x3C); //initialize with the I2C addr 0x3C (for the 128x32)(initializing the display)
+  //  Wire.begin();
+  //  display.begin(SSD1306_SWITCHCAPVCC, 0x3C); //initialize with the I2C addr 0x3C (for the 128x32)(initializing the display)
   pinMode(pumpPin, OUTPUT);
-  
+
   Serial.begin(9600); //(?) initialize serial and wait for port to open:
-  
+
+  //Real-time Clock
+  Rtc.Begin();
+  RtcDateTime compiled = RtcDateTime(__DATE__, __TIME__);
+  if (Rtc.GetIsWriteProtected()) { //RTC was write protected, enabling writing now
+    Rtc.SetIsWriteProtected(false);
+  }
+  if (!Rtc.GetIsRunning()) { //RTC was not actively running, starting now
+    Rtc.SetIsRunning(true);
+  }
+  RtcDateTime now = Rtc.GetDateTime();
+  if (now < compiled) { //RTC is older than compile time -> Updating DateTime
+    Rtc.SetDateTime(compiled);
+  }
+
+  //WiFi Connection
   while (!Serial) {
     ; //wait for serial port to connect (needed for native USB port only)
   }
-  //Check for the WiFi module:
+  //check for the WiFi module:
   if (WiFi.status() == WL_NO_MODULE) {
     Serial.println("Communication with WiFi module failed!");
     while (true); //don't continue
@@ -69,7 +92,7 @@ void setup() {
   if (fv < "1.0.0") {
     Serial.println("Please upgrade the firmware");
   }
-  //Attempt to connect to Wifi network:
+  //attempt to connect to Wifi network:
   while (status != WL_CONNECTED) {
     Serial.print("Attempting to connect to SSID: ");
     Serial.println(ssid);
@@ -81,11 +104,15 @@ void setup() {
 
 }
 
+
 void loop() {
   WiFiClient client = server.available(); //listen for incoming clients
 
-  //"Temperture" function  
-  if(!temp_reset()) {
+  //Real-time Clock
+  RtcDateTime now = Rtc.GetDateTime();
+
+  //"Temperture" function
+  if (!temp_reset()) {
     temp_write(0xCC); //"Skip_ROM"
     temp_write(0x44); //"Convert_T"
     delay(750); //(?)
@@ -100,15 +127,15 @@ void loop() {
     Serial.print("Temperature:");
     Serial.println(temperature);
   }
-//  displayTemp();
-//  display.display();
+  //  displayTemp();
+  //  display.display();
 
   //"Water Changing" function
   int turbidityValue = analogRead(turbidityPin);
   turbidityVol = turbidityValue * (5.0 / 1024.0); //convert the "read turbidity value" (0-1023) to voltage (0-5V)
   Serial.print("Turbidity Voltage:");
   Serial.print(turbidityVol);
-  if(turbidityVol < turbidityStandard) {
+  if (turbidityVol < turbidityStandard) {
     digitalWrite(pumpPin, HIGH); //turn ON pump
     Serial.println(" Pump ON");
     pumpOn = true;
@@ -126,7 +153,7 @@ void loop() {
       if (client.available()) {
         char c = client.read();
         Serial.write(c);
-        if (c == '\n' && currentLineIsBlank) 
+        if (c == '\n' && currentLineIsBlank)
         { //if reaching the end of line and the line is blank, the http request has ended, you can send a reply
           client.println("HTTP/1.1 200 OK"); //send a standard http response header
           client.println("Content-Type: text/html");
@@ -141,7 +168,22 @@ void loop() {
           client.println("<body>");
           client.println("<h1>Tortoise Smart Home</h1>");
 
-          //"Temperture"
+          //Real-time Clock
+          client.print("<h3>Current Time: ");
+          client.print(now.Day());
+          client.print("/");
+          client.print(now.Month());
+          client.print("/");
+          client.print(now.Year());
+          client.print(" ");
+          client.print(now.Hour());
+          client.print(":");
+          client.print(now.Minute());
+          client.print(":");
+          client.print(now.Second());
+          client.println("</h3>");
+
+          //"Temperature"
           client.println("<h2><mark><i>Temperature:</i></mark></h2>");
           client.print("<h3>Current water temperature is ");
           client.print(temperature);
@@ -155,15 +197,15 @@ void loop() {
           client.print("Standard water turbidity voltage is above ");
           client.print(turbidityStandard);
           client.println("V.");
-          if(pumpOn) {
+          if (pumpOn) {
             client.println("<h3>Pump is currently ON!</h3>");
           }
           else {
             client.println("<h3>Pump is currently OFF.</h3>");
           }
-          
+
           // END ~ ~ ~
-          
+
           client.println("</body>");
           client.println("</html>");
           break;
@@ -189,13 +231,13 @@ void loop() {
 void printWifiStatus() {
   Serial.print("SSID: ");
   Serial.println(WiFi.SSID()); //print the SSID of the network you're attached to
-  
+
   IPAddress ip = WiFi.localIP(); //print your board's IP address
   Serial.print("IP Address: ");
   Serial.println(ip);
 }
 
-//for OLED screen 
+//for OLED screen
 //void displayTemp(){
 //  delay(2000); //(?) website test~
 //  display.clearDisplay();
@@ -209,21 +251,21 @@ void printWifiStatus() {
 //  display.print(" C");
 //}
 
-//for water temperature sensor 
-unsigned char temp_reset() 
+//for water temperature sensor
+unsigned char temp_reset()
 {
   pinMode(tempPin, OUTPUT);
   digitalWrite(tempPin, LOW);
   delayMicroseconds(480);
   pinMode(tempPin, INPUT);
   delayMicroseconds(60);
-  if(digitalRead(tempPin) == 0) {
+  if (digitalRead(tempPin) == 0) {
     delayMicroseconds(480);
-    return 0; 
+    return 0;
   }
   else {
     delayMicroseconds(480);
-    return 1; 
+    return 1;
   }
 }
 
@@ -232,9 +274,8 @@ void temp_write(char WRT)
   char i = 0, Cmd = 0;
   Cmd = WRT;
   pinMode(tempPin, INPUT);
-  for(i = 0; i< 8; i++)
-  {
-    if((Cmd & (1<<i)) != 0) //Bitshift Left : variable<<number_of_bits
+  for (i = 0; i < 8; i++) {
+    if ((Cmd & (1 << i)) != 0) //Bitshift Left : variable<<number_of_bits
     {
       pinMode(tempPin, OUTPUT);
       digitalWrite(tempPin, LOW);
@@ -242,12 +283,11 @@ void temp_write(char WRT)
       pinMode(tempPin, INPUT);
       delayMicroseconds(60);
     }
-    else
-    {
+    else {
       pinMode(tempPin, OUTPUT);
       digitalWrite(tempPin, LOW);
       delayMicroseconds(60);
-      pinMode(tempPin, INPUT);      
+      pinMode(tempPin, INPUT);
     }
   }
 }
@@ -256,41 +296,14 @@ unsigned char temp_read()
 {
   char i = 0, result = 0;
   pinMode(tempPin, INPUT);
-  for(i = 0; i < 8; i++)
-  {
+  for (i = 0; i < 8; i++) {
     pinMode(tempPin, OUTPUT);
     digitalWrite(tempPin, LOW);
-    delayMicroseconds(2); 
+    delayMicroseconds(2);
     pinMode(tempPin, INPUT);
-    if(digitalRead(tempPin) != 0)
-      result |= 1<<i;
+    if (digitalRead(tempPin) != 0)
+      result |= 1 << i;
     delayMicroseconds(60);
   }
   return result;
 }
-
-//for real-time clock module
-//String weekDay(uint8_t dayofweek) //to show dayOfWeek in short-form string 
-//{
-//  if(dayofweek == 1){
-//    return "Mon";
-//  }
-//  else if(dayofweek == 2){
-//    return "Tue";
-//  }
-//  else if(dayofweek == 3){
-//    return "Wed";
-//  }
-//  else if(dayofweek == 4){
-//    return "Thu";
-//  }
-//  else if(dayofweek == 5){
-//    return "Fri";
-//  }
-//  else if(dayofweek == 6){
-//    return "Sat";
-//  }
-//  else if(dayofweek == 7){
-//    return "Sun";
-//  }
-//}
